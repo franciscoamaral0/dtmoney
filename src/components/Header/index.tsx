4a735966a97dbc09d64151b406ad85489b5c6555
franciscoamaral0 <franciscogurgel@me.com>
2021-12-18 23:58:10 +0000

    add modal and forms

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,25 @@
+import { useState } from 'react'
 import logoImg from '../../assets/Logo.svg'
+import Modal from 'react-modal'
 import { Container, Content } from './styles'
 
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
 
-export function Header(){
+export function Header({onOpenNewTransactionModal}:HeaderProps){
+
+  
+
   return(
     <Container>
       <Content>
         <img  src={logoImg} alt="dt money" />
-        <button type="button">
+        <button onClick={onOpenNewTransactionModal} >
           Nova Transação
         </button>
+
+        
       </Content>
     </Container>
 
